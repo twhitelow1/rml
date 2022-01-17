22e4a8a827d2f4f1b95781ee358ad8a37b8f9d73
Todd Whitelow <todd@whitelow.tech>
2022-01-17 00:46:41 -0700

    add submit prevent to add functionality to the form

--- src/views/ComingSoon.vue
+++ src/views/ComingSoon.vue
@@ -80,10 +80,12 @@
                                 <p class="dugem-text"> Talent looking for a talent buyer? Venue looking for a promoter to book talent? Looking to get involved? Want to say hello? </p>
                             </div>
                             <div class="contact-form-style col-span-6 sm:col-span-12 res:col-span-12">
-                                <input type="text" name="ame" size="40" placeholder="Name" v-model="name">
-                                <input type="text" name="email" size="40" placeholder="Email" v-model="email">
-                                <textarea name="message" cols="40" rows="10" placeholder="Text" v-model="message"></textarea>
-                                <input type="submit" value="Send" class="button-basic-1">
+                                <form @submit.prevent="sendEmail">
+                                  <input type="text" name="ame" size="40" placeholder="Name" v-model="name">
+                                  <input type="text" name="email" size="40" placeholder="Email" v-model="email">
+                                  <textarea name="message" cols="40" rows="10" placeholder="Text" v-model="message"></textarea>
+                                  <input type="submit" value="Send" class="button-basic-1">
+                                </form>
 
                             </div>
                         </div>
