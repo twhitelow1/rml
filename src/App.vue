1b31ea153cce145171226556724d00534abd7c42
Todd Whitelow <todd@whitelow.tech>
2022-01-16 23:32:11 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,106 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  
+        <!--MAIN WRAPPER START-->
+        <div class="main-wrapper clearfix">
+            <!-- HEADER START
+                ============================================= -->
+            <header id="header" class="header-style-1">
+                <div class="container">
+                    <div class="row clearfix">
+                        <div class="header-clear clearfix">
+                            <div class="header-content-center clearfix">
+                                <!-- LOGO START
+                                ============================================= -->
+                                <div class="logo">
+                                    <a href="/index.html"><img src="/img/rml-logo.png" style="padding-top:0px; width: 150px;" alt="Rocky Mountain Live Logo" /></a>
+                                </div>
+                                <!-- LOGO END -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row clearfix">
+                        <div class="header-clear clearfix">
+                            <div class="header-content-center clearfix">
+                                <!-- NAVIGATION START============================================= -->
+                                <nav class="main-nav">
+                                    <!-- Mobile menu toggle button (hamburger/x icon) -->
+                                    <input id="main-menu-state" type="checkbox" />
+                                    <label class="main-menu-btn sub-menu-trigger" for="main-menu-state">
+                                        <span class="main-menu-btn-icon"></span> Toggle main menu visibility
+                                    </label>
+                                    <!-- Sample menu definition -->
+                                    <ul id="main-menu" class="sm sm-clean">
+                                        <li class="menu-item"><a href="/index.html" class="menu-link active">Home</a></li>
+                                        <li class="menu-item"><a href="/about.html" class="menu-link">About</a></li>
+                                        <li class="menu-item"><a href="/event.html" class="menu-link">Event</a></li>
+                                        <li class="menu-item"><a href="/blog.html" class="menu-link">News</a></li>
+                                        <li class="menu-item"><a href="/contact.html" class="menu-link">Contact</a></li>
+                                    </ul>
+                                </nav>
+                                <!-- NAVIGATION END -->
+                            </div>
+                            <div class="header-content-right">
+                                <div class="header-info head-item clearfix">
+                                    <div class="info-wrap">
+                                        <div class="search-wrap">
+                                            <button id="btn-search" class="btn btn--search"><i class="icon-simple-line-icons-143"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="search">
+                                <button id="btn-search-close" class="btn--search-close"><i class="icon-themify-1"></i></button>
+                                <form class="search__form">
+                                    <input class="search__input" name="search" type="search" placeholder="Search..." />
+                                    <span class="search__info">Hit enter to search or ESC to close</span>
+                                </form>
+                            </div><!-- /search -->
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- HEADER END -->
+            <!-- SECTION -->
+
     <router-view/>
+    <!-- FOOTER START
+                ============================================= -->
+            <footer id="footer" class="footer clearfix">
+                <div class="footer-wrap clearfix">
+                    <div class="footer-bottom clearfix">
+                        <div class="container">
+                            <div class="row">
+                                <div class="foot-col column column-1 text-center clearfix">
+                                    <div class="logo-footer">
+                                        <a href="/index.html"><img src="/img/rml-logo.png" style="width: 50%;" stalt="Logo-Footer" /></a>
+                                    </div>
+                                </div>
+
+                                <div class="foot-col column column-1 text-center clearfix">
+                                    <div class="social-footer">
+                                        <ul>
+                                            <li class="soc-icon"><a href="#"><i class="icon icon-themify-13"></i>Twitter</a></li>
+                                            <li class="soc-icon"><a href="#"><i class="icon icon-themify-17"></i>Facebook</a></li>
+                                            <li class="soc-icon"><a href="#"><i class="icon icon-themify-11"></i>Google+</a></li>
+                                            <li class="soc-icon"><a href="#"><i class="icon icon-social-dribbble"></i>Dribbble</a></li>
+                                            <li class="soc-icon"><a href="#"><i class="icon icon-behance2"></i>Behance</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="foot-col column column-1 text-center clearfix">
+                                    <div id="copyright" class="copyright-text">
+                                        © Copyright 2021, Built by ThemesAwesome All rights reserved.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- FOOTER END -->
+        </div>
+        <!-- MAIN WRAPPER END -->
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
